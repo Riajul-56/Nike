2c7ccc2ab849f54012999a26fa071de2ec4df518
Riajul-56 <riajulhasansiam@gmail.com>
2025-04-19 22:08:38 +0600

    Remove commented-out constructor and documentation from ApiError class

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,12 +1,16 @@
 class ApiError extends Error {
-    construstor(statuscode, message, erros) {
-        super(message);
-        this.success = false;
-        this.statuscode = statuscode;
-        this.message = message ?? "Something want wrong";
-        this.erros = erros ?? {};
-        this.stack = Error.captureStackTrace(this, this.construstor);
+    // construstor(statuscode, message, erros) {
+    //     super(message);
+    //     this.success = false;
+    //     this.statuscode = statuscode;
+    //     this.message = message ?? "Something want wrong";
+    //     this.erros = erros ?? {};
+    //     this.stack = Error.captureStackTrace(this, this.construstor);
 
-    }
+    // }
+
+//  @param {number} statuscode -the HTTP status code for the error 
+//  @param {string} message  -the error message 
+
 }
 export default ApiError;
