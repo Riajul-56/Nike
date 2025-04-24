dc727012c4ccb83d7709469993eb3cdf7714811f
Riajul-56 <riajulhasansiam@gmail.com>
2025-04-24 17:45:27 +0600

    Refactor ApiError class constructor and improve error handling methods

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,27 +1,56 @@
 class ApiError extends Error {
-    // construstor(statuscode, message, erros) {
-    //     super(message);
-    //     this.success = false;
-    //     this.statuscode = statuscode;
-    //     this.message = message ?? "Something want wrong";
-    //     this.erros = erros ?? {};
-    //     this.stack = Error.captureStackTrace(this, this.construstor);
-
-    // }
 
     //  @param {number} statuscode -the HTTP status code for the error 
     //  @param {string} message  -the error message 
     //  @param {object} erros -additional error details (optional)
 
 
-    constructor(statuscode, message = "Somthing went wrong ", erros = {}, stack = " ", arrocode = '') {
+    constructor(statuscode, message = "Somthing went wrong ", errors = {}, stack = " ", errorCode = '') {
         super(message);
-        this.name=this.constructor.name;
-        this.success=false;
-        this.statuscode=statuscode;
-        
+
+        this.success = false;
+        this.statuscode = statuscode;
+        this.message = message;
+        this.errors = errors;
+        this.errorCode = errorCode;
+
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
     }
 
+    //401 unauthorized
+    static unauthorized(message = "Unauthorized", errors = {}, errorCode = "AUTH_401",) {
+        return new ApiError(401, message, errors, " ", errorCode)
+    }
+
+    //403 forbidden
+    static forbidden(message = "Forbidden", errors = {}, errorCode = "AUTH_403",) {
+        return new ApiError(403, message, errors, " ", errorCode)
+    }
+
+    //400 Bad Request
+    static badRequest(message = "Bad Request", errors = {}, errorCode = "REQ_400",) {
+        return new ApiError(400, message, errors, " ", errorCode)
+    }
+
+    //404 Not Found
+    static notFound(message = "Not Found", errors = {}, errorCode = "REQ_404",) {
+        return new ApiError(404, message, errors, " ", errorCode)
+    }
+
+    //409 Conflict
+    static conflict(message = "Conflict", errors = {}, errorCode = "REQ_409",) {
+        return new ApiError(409, message, errors, " ", errorCode)
+    }
+
+    //500 Server Error 
+    static serverError(message = "Internal Server Error ", errors = {}, errorCode = "SERVER_500",) {
+        return new ApiError(500, message, errors, " ", errorCode)
+    }
 
 }
 export default ApiError;
