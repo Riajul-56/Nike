a9f20863016b3b5985e80651cdfaaf6279a43394
Riajul-56 <riajulhasansiam@gmail.com>
2025-05-20 23:35:59 +0600

    add validator

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -2,7 +2,6 @@ import { User } from "../../models/user.model.js";
 import { asyncHandler } from "../../utils/asynceHandler.js";
 
 const signup = asyncHandler(async (req, res) => {
-    const user = await User.create(req.body)
-    return res.json({ user })
+    const {username,name,email,password}=req.body
 })
 export { signup }
