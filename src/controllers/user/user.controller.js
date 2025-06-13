909fd0c68cc1f6d4ecc29b90f989638c398c1cd6
Riajul-56 <riajulhasansiam@gmail.com>
2025-06-13 15:01:45 +0600

    Add cloudinary configuration and file upload utility; implement avatar upload functionality in user controller

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -1,8 +1,10 @@
+import { overwrite } from "zod/v4";
 import { APP_URL, GOOGLE_ACCESS_TOKEN_URL, GOOGLE_CALLBACK_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_OAUTH_SCOPES, GOOGLE_OAUTH_URL, JWT_SECRET, GOOGLE_TOKEN_INFO_URL } from "../../constants.js";
 import { User } from "../../models/user.model.js";
 import ApiError from "../../utils/apiError.js";
 import ApiSuccess from "../../utils/apiSuccess.js";
 import { asyncHandler } from "../../utils/asynceHandler.js";
+import { fileUpload } from "../../utils/fileupload.js";
 import { sendMail, forgotPasswordFormat, verifyEmailFormat } from "../../utils/mail.js";
 import jwt from "jsonwebtoken"
 
@@ -310,7 +312,21 @@ const resetpassword = asyncHandler(async (req, res) => {
 
 const avatarUpload = asyncHandler(async (req, res) => {
     const avatar = req.file
-    return res.status(200).json(ApiSuccess.ok('Avatar uploaded', avatar))
+    const user=req.user
+    const result = await fileUpload(avatar.path, {
+        folder: 'avatar',
+        use_filename: true,
+        overwrite:true,
+        resource_type: 'image',
+        transformation: [{ width: 300, height: 300, crop: 'fill', gravity: 'face', radius: 'max' }],
+        public_id: req.user._id
+    })
+    user.avatar={
+        public_id:result.public_id,
+        url:result.secure_url
+    }
+    return res.status(200).json(ApiSuccess.ok('Avatar uploaded', user
+    ))
 })
 
 
--- src/utils/fileupload.js
+++ src/utils/fileupload.js
@@ -1,42 +1,32 @@
 import { v2 as cloudinary } from 'cloudinary';
+import { CLOUD_API_KEY, CLOUD_API_SECRET, CLOUD_NAME } from '../constants.js';
+import ApiError from './apiError.js';
+import { unlinkSync } from 'fs'
 
-(async function () {
 
-    // Configuration
-    cloudinary.config({
-        cloud_name: 'dvupk8boe',
-        api_key: '687552613645942',
-        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
-    });
+cloudinary.config({
+    cloud_name: CLOUD_NAME,
+    api_key: CLOUD_API_KEY,
+    api_secret: CLOUD_API_SECRET
+});
 
-    // Upload an image
-    const uploadResult = await cloudinary.uploader
-        .upload(
-            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-            public_id: 'shoes',
-        }
-        )
-        .catch((error) => {
-            console.log(error);
-        });
+// Upload an image
+const fileUpload = async (file, option) => {
+    try {
+        const data = await cloudinary.uploader.upload(file, { ...option })
+        unlinkSync(file)
+        return data
+    } catch (error) {
+        unlinkSync(file)
+        throw ApiError.serverError(error.message)
+    }
+}
+export { fileUpload }
 
-    console.log(uploadResult);
+// // Optimize delivery by resizing and applying auto-format and auto-quality
+// const optimizeUrl = cloudinary.url('shoes', {
+//     fetch_format: 'auto',
+//     quality: 'auto'
+// });
 
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-
-    console.log(optimizeUrl);
-
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-
-    console.log(autoCropUrl);
-})();
+// console.log(optimizeUrl);
