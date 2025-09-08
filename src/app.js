bf998b76ae88777160f36eb8f4c360d8be5590b1
Riajul-56 <riajulhasansiam@gmail.com>
2025-09-08 14:32:38 +0600

    add express-rate-limit package and it's implement

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { WHITELIST } from './constants.js';
+import { ACCESS_TOKEN_SECRET, WHITELIST } from './constants.js';
 import cookieParser from 'cookie-parser';
 import errorHandler from './middlewares/errorHandler.middleware.js';
 
@@ -16,15 +16,26 @@ app.use(
   })
 );
 
-app.use(cookieParser());
+app.use(cookieParser(ACCESS_TOKEN_SECRET));
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: (req, res) => (req.user ? 100 : 10),
+  standardHeaders: 'draft-8',
+  legacyHeaders: true,
+  message: 'To many requests from this IP,please try again after 15 minutes ',
+  keyGenerator: req => req.ip,
+});
+
+app.use(limiter);
 //define routes
 import healthCheckRoute from './routes/healthCheck.route.js';
 import userRoute from './routes/user.route.js';
 import categoryRoute from './routes/category.route.js';
 import subcategoryRoute from './routes/subcategory.route.js';
+import rateLimit from 'express-rate-limit';
 
-app.use(healthCheckRoute);
+app.use('/api/v1', healthCheckRoute);
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1', categoryRoute);
 app.use('/api/v1', subcategoryRoute);
